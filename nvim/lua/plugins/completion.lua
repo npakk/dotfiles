babdf2e58af5f3511cd513a62560a7aacec6b5af
Npakk <npakk669@gmail.com>
2021-06-04 02:23:56 +0900

    change lua setting

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,7 +1,6 @@
+vim.g.completion_enable_snippet = 'snippets.nvim'
 vim.g.completion_chain_complete_list = {
   default = {
     {complete_items = {'lsp','snippet', 'buffers'}}
   },
 }
-vim.g.completion_enable_snippet = 'snippets.nvim'
-require'lspconfig'.solargraph.setup{on_attach=require'completion'.on_attach}
--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -1,3 +1,8 @@
+-- fix highlight
+require'hop'.setup {
+  create_hl_autocmd = true
+}
+
 vim.api.nvim_set_keymap('n', '<leader>w', '<cmd>HopWord<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>HopChar1<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', '<leader>s', '<cmd>HopChar1<CR>', { noremap = true, silent = true })
