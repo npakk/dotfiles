3cdd8d52301c60ede8eb23f4ba0510f1f491dbc1
Npakk <npakk669@gmail.com>
2021-06-14 02:41:08 +0900

    treesitter indent disable'html' and enable'ruby' | snippets ruby 'do' change

--- nvim/lua/plugins/emmet.lua
+++ nvim/lua/plugins/emmet.lua
@@ -1,2 +1,2 @@
 vim.g.user_emmet_install_global = 0
-vim.cmd('autocmd FileType html,css,eruby,vue EmmetInstall')
+vim.cmd('autocmd FileType html,css,markdown,eruby,vue EmmetInstall')
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -28,7 +28,7 @@ require'nvim-treesitter.configs'.setup {
 
   indent = {
     enable = true,
-    disable = { "ruby" },
+    disable = { "html" },
   },
 
   textobjects = {
