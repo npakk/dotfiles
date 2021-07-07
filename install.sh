a776f5582a60a60c154832aa6d0e84090eff049b
Npakk <npakk669@gmail.com>
2021-07-08 00:38:52 +0900

    change directory

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env zsh
 
 # Homebrew
-ln -sf ~/dotfiles/Brewfile ~/Brewfile
+ln -sf ~/dotfiles/HOME/Brewfile ~/Brewfile
 
 # zsh
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/zsh/.zshrc ~/.config/zsh/.zshrc
+ln -sf ~/dotfiles/HOME/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.config/zsh/.zshrc ~/.config/zsh/.zshrc
 
 # git
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/git/ignore ~/.config/git/ignore
+ln -sf ~/dotfiles/HOME/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.config/git/ignore ~/.config/git/ignore
 
 # ghq
 if [ ! -d ~/.config/gh ]; then
   mkdir ~/.config/gh
 fi
-ln -sf ~/dotfiles/gh/config.yml ~/.config/gh/config.yml
+ln -sf ~/dotfiles/.config/gh/config.yml ~/.config/gh/config.yml
 
 # karabiner-elements
-ln -sf ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-ln -snf ~/dotfiles/karabiner/assets ~/.config/karabiner/assets
+ln -sf ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+ln -snf ~/dotfiles/.config/karabiner/assets ~/.config/karabiner/assets
 
 # iTerm2
-ln -sf ~/dotfiles/iTerm2/com.googlecode.iterm2.plist ~/.config/iTerm2/com.googlecode.iterm2.plist
-ln -sf ~/dotfiles/iTerm2/iceberg_bluemoon.itermcolors ~/.config/iTerm2/iceberg_bluemoon.itermcolors
-ln -sf ~/dotfiles/iTerm2/MyProfile.json ~/.config/iTerm2/MyiTerm.json
+ln -sf ~/dotfiles/.config/iTerm2/com.googlecode.iterm2.plist ~/.config/iTerm2/com.googlecode.iterm2.plist
+ln -sf ~/dotfiles/.config/iTerm2/iceberg_bluemoon.itermcolors ~/.config/iTerm2/iceberg_bluemoon.itermcolors
+ln -sf ~/dotfiles/.config/iTerm2/MyProfile.json ~/.config/iTerm2/MyiTerm.json
 
 # neovim
-ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
-ln -snf ~/dotfiles/nvim/after ~/.config/nvim/after
-ln -snf ~/dotfiles/nvim/lua ~/.config/nvim/lua
-ln -snf ~/dotfiles/nvim/snippets ~/.config/nvim/snippets
+ln -sf ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -snf ~/dotfiles/.config/nvim/after ~/.config/nvim/after
+ln -snf ~/dotfiles/.config/nvim/lua ~/.config/nvim/lua
+ln -snf ~/dotfiles/.config/nvim/snippets ~/.config/nvim/snippets
