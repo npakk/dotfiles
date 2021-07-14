00316325903aa98c10bc420a1a28e690309fda39
npakk <npakk669@gmail.com>
2021-07-15 00:39:09 +0900

    appearances delete concat

--- config/nvim/lua/core/appearances.lua
+++ config/nvim/lua/core/appearances.lua
@@ -1,3 +1,3 @@
-vim.cmd([[execute "packadd " . "lush.nvim"]])
-vim.cmd([[execute "packadd " . "gruvbox.nvim"]])
-vim.cmd([[execute "colorscheme " . "gruvbox"]])
+vim.cmd([[execute "packadd lush.nvim"]])
+vim.cmd([[execute "packadd gruvbox.nvim"]])
+vim.cmd([[execute "colorscheme gruvbox"]])
