818ae4f6d1dc6f54ab35cffdb71c1278a9e94609
npakk <npakk669@gmail.com>
2021-08-24 20:17:08 +0900

    change colorscheme gruvbox

--- config/nvim/lua/core/appearances.lua
+++ config/nvim/lua/core/appearances.lua
@@ -1,16 +1,16 @@
---vim.api.nvim_exec(
---  [[
---  packadd lush.nvim
---  packadd gruvbox.nvim
---  colorscheme gruvbox
---  ]],
---  true
---)
 vim.api.nvim_exec(
   [[
-  let g:nvcode_termcolors=256
-  packadd nvcode-color-schemes.vim
-  colorscheme nvcode
+  packadd lush.nvim
+  packadd gruvbox.nvim
+  colorscheme gruvbox
   ]],
   true
 )
+-- vim.api.nvim_exec(
+--   [[
+--   let g:nvcode_termcolors=256
+--   packadd nvcode-color-schemes.vim
+--   colorscheme nvcode
+--   ]],
+--   true
+-- )
