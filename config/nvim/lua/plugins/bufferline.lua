171b460acf711954707f4250d183456329ca9e93
npakk <npakk669@gmail.com>
2021-09-01 19:04:41 +0900

    bufferline change setting

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -2,6 +2,15 @@ local M = {}
 
 function M.setup()
   local kopts = { noremap = true, silent = true }
+  vim.api.nvim_set_keymap("n", "<leader>1", [[<cmd>BufferLineGoToBuffer 1<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>2", [[<cmd>BufferLineGoToBuffer 2<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>3", [[<cmd>BufferLineGoToBuffer 3<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>4", [[<cmd>BufferLineGoToBuffer 4<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>5", [[<cmd>BufferLineGoToBuffer 5<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>6", [[<cmd>BufferLineGoToBuffer 6<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>7", [[<cmd>BufferLineGoToBuffer 7<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>8", [[<cmd>BufferLineGoToBuffer 8<CR>]], kopts)
+  vim.api.nvim_set_keymap("n", "<leader>9", [[<cmd>BufferLineGoToBuffer 9<CR>]], kopts)
   vim.api.nvim_set_keymap("n", "<Right>", [[:BufferLineCycleNext<CR>]], kopts)
   vim.api.nvim_set_keymap("n", "<Left>", [[:BufferLineCyclePrev<CR>]], kopts)
   vim.api.nvim_set_keymap("n", "<Down>", [[:BufferLineMovePrev<CR>]], kopts)
@@ -10,12 +19,15 @@ function M.setup()
   vim.api.nvim_set_keymap("n", "<leader><Left>", [[:BufferLineCloseLeft<CR>]], kopts)
 end
 
+-- numbers = "ordinal",
+-- number_style = "",
+-- mappings = true,
 function M.config()
   require("bufferline").setup({
     options = {
-      numbers = "ordinal",
-      number_style = "",
-      mappings = true,
+      numbers = function(opts)
+        return string.format("%s|%s", opts.ordinal, opts.raise(opts.id))
+      end,
       diagnostics = "nvim_lsp",
       diagnostics_indicator = function(_, _, diagnostics_dict)
         local s = " "
