866cf36d70bcc4ccfb8570de46dd115e70fa72c5
npakk <npakk669@gmail.com>
2021-09-03 20:49:54 +0900

    gitsigns setting separate

--- config/nvim/lua/plugins/packer.lua
+++ config/nvim/lua/plugins/packer.lua
@@ -275,63 +275,7 @@ local function init()
 
   use({
     "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add = { hl = "GitGutterAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-          change = {
-            hl = "GitGutterChange",
-            text = "│",
-            numhl = "GitSignsChangeNr",
-            linehl = "GitSignsChangeLn",
-          },
-          delete = {
-            hl = "GitGutterDelete",
-            text = "_",
-            numhl = "GitSignsDeleteNr",
-            linehl = "GitSignsDeleteLn",
-          },
-          topdelete = {
-            hl = "GitGutterDelete",
-            text = "‾",
-            numhl = "GitSignsDeleteNr",
-            linehl = "GitSignsDeleteLn",
-          },
-          changedelete = {
-            hl = "GitGutterChange",
-            text = "~",
-            numhl = "GitSignsChangeNr",
-            linehl = "GitSignsChangeLn",
-          },
-        },
-        keymaps = {
-          noremap = true,
-          buffer = true,
-          ["n ]g"] = {
-            expr = true,
-            [[&diff ? "]g" : "<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>"]],
-          },
-          ["n [g"] = {
-            expr = true,
-            [[&diff ? "[g" : "<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>"]],
-          },
-
-          ["n <leader>hs"] = [[<cmd>lua require"gitsigns".stage_hunk()<CR>]],
-          ["v <leader>hs"] = [[<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>]],
-          ["n <leader>hu"] = [[<cmd>lua require"gitsigns".undo_stage_hunk()<CR>]],
-          ["n <leader>hr"] = [[<cmd>lua require"gitsigns".reset_hunk()<CR>]],
-          ["v <leader>hr"] = [[<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>]],
-          ["n <leader>hR"] = [[<cmd>lua require"gitsigns".reset_buffer()<CR>]],
-          ["n <leader>hp"] = [[<cmd>lua require"gitsigns".preview_hunk()<CR>]],
-          ["n <leader>hb"] = [[<cmd>lua require"gitsigns".blame_line(true)<CR>]],
-
-          -- Text objects
-          ["o ih"] = [[:<C-U>lua require"gitsigns.actions".select_hunk()<CR>]],
-          ["x ih"] = [[:<C-U>lua require"gitsigns.actions".select_hunk()<CR>]],
-        },
-        sign_priority = 6,
-      })
-    end,
+    config = require("plugins.gitsigns").config,
     requires = {
       { "nvim-lua/plenary.nvim" },
     },
