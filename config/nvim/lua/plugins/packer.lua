75c0d50a7adc8456e1fad38385f0800eb8f3fdaa
npakk <npakk669@gmail.com>
2021-09-10 18:46:40 +0900

    change vim kommentary lua single comment setting

--- config/nvim/lua/plugins/packer.lua
+++ config/nvim/lua/plugins/packer.lua
@@ -191,12 +191,16 @@ local function init()
   use({
     "b3nj5m1n/kommentary",
     config = function()
-      require("kommentary.config").configure_language("default", {
+      local kommentary = require("kommentary.config")
+      kommentary.configure_language("default", {
         single_line_comment_string = "auto",
         multi_line_comment_strings = "auto",
         hook_function = function()
           require("ts_context_commentstring.internal").update_commentstring()
         end,
+      })
+      kommentary.configure_language("lua", {
+        prefer_single_line_comments = true,
       })
     end,
   })
