35ae7053ce829b54853b84a983d792a9d394ca33
npakk <npakk669@gmail.com>
2025-02-22 21:26:09 +0900

    doom change

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -21,8 +21,13 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "HackGen Console NF" :size 30 :weight 'regular)
-     doom-variable-pitch-font (font-spec :family "HackGen Console NF" :size 31))
+(when (eq system-type 'windows-nt)
+  (setq doom-font (font-spec :family "HackGen Console NF" :size 30 :weight 'regular)
+        doom-variable-pitch-font (font-spec :family "HackGen Console NF" :size 31)))
+
+(when (eq system-type 'darwin)
+  (setq doom-font (font-spec :family "HackGen Console NF" :size 20 :weight 'regular)
+        doom-variable-pitch-font (font-spec :family "HackGen Console NF" :size 21)))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
