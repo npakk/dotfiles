667b6505bd6d55ef84a8bb0365dee9fddadfe01b
npakk <npakk669@gmail.com>
2025-09-03 13:42:52 +0900

    背景色を透過

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -4,6 +4,12 @@ return {
     lazy = false,
     config = function()
       vim.cmd([[colorscheme iceberg]])
+      vim.cmd([[
+      highlight Normal guibg=none
+      highlight NonText guibg=none
+      highlight Normal ctermbg=none
+      highlight NonText ctermbg=none
+      ]])
     end,
   },
   {
