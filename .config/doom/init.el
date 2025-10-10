8e26f728ffd2530412975ce5170d14edff87b17a
npakk <npakk669@gmail.com>
2025-10-10 09:14:47 +0900

    change

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -18,7 +18,7 @@
 (doom! :input
        ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
-       japanese
+       ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
