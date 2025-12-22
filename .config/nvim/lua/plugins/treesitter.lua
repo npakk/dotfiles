fc394f8ac376e6f39864ff4dd96b5be305644596
npakk <npakk669@gmail.com>
2025-12-22 14:54:35 +0900

    change tree-sitter settings

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,30 +3,28 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "main",
+      keys = {
+        {
+          "<leader>s",
+          "<cmd>lua require('nvim-treesitter-textobjects.swap').swap_next('@parameter.inner')<CR>",
+          desc = "Swap Next",
+        },
+        {
+          "<leader>S",
+          "<cmd>lua require('nvim-treesitter-textobjects.swap').swap_previous('@parameter.inner')<CR>",
+          desc = "Swap Previous",
+        },
+      },
     },
-    event = "BufReadPre",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "markdown",
-          "query",
-          "vimdoc",
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        textobjects = {
-          swap = {
-            enable = true,
-            swap_next = { ["<leader>s"] = "@parameter.inner" },
-            swap_previous = { ["<leader>S"] = "@parameter.inner" },
-          },
-        },
+      require("nvim-treesitter").install({
+        "lua",
+        "markdown",
+        "query",
+        "vimdoc",
       })
     end,
   },
