3370ba18f8f5f1096bf67e623357bb89a0ed5610
npakk <npakk669@gmail.com>
2026-02-12 15:02:46 +0900

    chg doom font size

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -23,8 +23,8 @@
 ;;
 (cond
  ((eq system-type 'darwin)
-  (setq doom-font (font-spec :family "HackGen Console NF" :size 19 :weight 'regular)
-        doom-variable-pitch-font (font-spec :family "HackGen Console NF" :size 20)))
+  (setq doom-font (font-spec :family "HackGen Console NF" :size 18 :weight 'regular)
+        doom-variable-pitch-font (font-spec :family "HackGen Console NF" :size 19)))
  ((eq system-type 'windows-nt)
   (setq doom-font (font-spec :family "HackGen Console NF" :size 24 :weight 'regular)
         doom-variable-pitch-font (font-spec :family "HackGen Console NF" :size 25))
